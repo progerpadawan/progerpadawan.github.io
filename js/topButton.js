421854c7dc31a49bbc60e7e13cf94e424d574efa
mzelensky <max.m.zelensky@gmail.com>
2020-05-26 16:41:10 +0200

    init commit

--- js/topButton.js
+++ js/topButton.js
@@ -1,4 +1,26 @@
 ﻿function topButtonFunction() {
-		  document.body.scrollTop = 0; // For Safari
-		  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-		}
+	var ua = navigator.userAgent.toLowerCase();
+	if (ua.indexOf('safari') != -1) {
+		SmoothVerticalScrolling(document.body, 5000, "top");
+	}else {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+}
+
+function SmoothVerticalScrolling(e, time, where) {
+    var eTop = e.getBoundingClientRect().top;
+    var eAmt = eTop / 1;
+    var curTime = 0;
+    while (curTime <= time) {
+        window.setTimeout(SVS_B, curTime, eAmt, where);
+        curTime += time / 1;
+    }
+}
+
+function SVS_B(eAmt, where) {
+    if(where == "center" || where == "")
+        window.scrollBy(0, eAmt / 2);
+    if (where == "top")
+        window.scrollBy(0, eAmt);
+}
